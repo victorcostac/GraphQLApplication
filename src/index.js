0c0f2e6c9d4d4136e65b82172af3b58b651d2791
victorcostac <107112671+victorcostac@users.noreply.github.com>
2023-10-26 18:35:25 -0300

    teste query e mutation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { ApolloServer, gql } = require("apollo-server");
 
-
 // Toda request é POST
 // Toda request bate no MESMO endpoint (/graphql)
 
@@ -10,14 +9,55 @@ const { ApolloServer, gql } = require("apollo-server");
 
 
 const typeDefs = gql`
+    type User{
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+    type Post{
+        _id: ID!
+        title: String!
+        content: String!
+        author: User!
+    }
+
 
     type Query{
         hello: String
+        users: [User!]!
+        getUserByEmail(email: String!): User!
+    }
+    type Mutation{
+        createUser(name: String!, email: String!): User!
     }
 `;
+const users = [
+    { _id:  String(Math.random()), name: "victor", email: "victor@gmail.com", active: true },
+    { _id:  String(Math.random()), name: "victor2", email: "victor2@gmail.com", active: true },
+    { _id:  String(Math.random()), name: "victor3", email: "victor3@gmail.com", active: true }
+];
+console.log(users)
+
 const resolvers = {
     Query: {
-        hello: () => 'Hello world'
+        hello: () => 'Hello world',
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find((users) => users.email === args.email) 
+        }
+    },
+    Mutation: {
+        createUser: (_,args) => {
+            const newUser = {
+                _id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true,
+            };
+            users.push(newUser);
+            return newUser;
+        },
     }
 };
 
